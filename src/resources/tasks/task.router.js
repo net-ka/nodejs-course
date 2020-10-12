74e7f634f23ac9c6ab8102c210e42eddd387c72f
Anna Fedosova <a.k.fedosova@gmail.com>
2020-10-12 14:53:34 +0300

    feature: add mergeParams to task route

--- src/resources/tasks/task.router.js
+++ src/resources/tasks/task.router.js
@@ -1,23 +1,22 @@
-const router = require('express').Router();
+const router = require('express').Router({ mergeParams: true });
 const Task = require('./task.model');
 const tasksService = require('./task.service');
 
-router.route('/:boardId/tasks').get(async (req, res) => {
+router.route('/').get(async (req, res) => {
   const tasks = await tasksService.getAllByBoard(req.params.boardId);
   res.json(tasks.map(Task.toResponse));
 });
 
-router.route('/:boardId/tasks/:taskId').get(async (req, res) => {
+router.route('/:taskId').get(async (req, res) => {
   try {
     const task = await tasksService.getById(req.params.taskId);
     res.json(Task.toResponse(task));
-    // res.json(task);
   } catch (e) {
     res.status(404).send(e.message);
   }
 });
 
-router.route('/:boardId/tasks').post(async (req, res) => {
+router.route('/').post(async (req, res) => {
   const task = await tasksService.create(
     new Task({
       ...req.body,
@@ -28,7 +27,7 @@ router.route('/:boardId/tasks').post(async (req, res) => {
   res.json(Task.toResponse(task));
 });
 
-router.route('/:boardId/tasks/:taskId').delete(async (req, res) => {
+router.route('/:taskId').delete(async (req, res) => {
   try {
     await tasksService.deleteById(req.params.taskId);
     res.status(204).send();
@@ -37,7 +36,7 @@ router.route('/:boardId/tasks/:taskId').delete(async (req, res) => {
   }
 });
 
-router.route('/:boardId/tasks/:taskId').put(async (req, res) => {
+router.route('/:taskId').put(async (req, res) => {
   try {
     const task = await tasksService.update(req.params.taskId, req.body);
     res.json(Task.toResponse(task));
